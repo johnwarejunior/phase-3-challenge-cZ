4b2847357b2967794ac8714d2cb9f58332fac03f
johnware15 <contact@johndware.com>
2017-10-02 15:31:28 -0700

    Schema designed

---
+++ part-2/schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF EXISTS hotel_db;
+CREATE DATABASE hotel_db;
+
+\c hotel_db
+
+DROP TABLE IF EXISTS guests
+CREATE TABLE guests(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50),
+  email VARCHAR(75) UNIQUE NOT NULL
+);
+
+DROP TABLE IF EXISTS rooms
+CREATE TABLE rooms(
+  id SERIAL PRIMARY KEY,
+  room_num VARCHAR(20),
+  capacity INTEGER
+  available BOOLEAN DEFAULT true
+);
+
+DROP TABLE IF EXISTS bookings
+CREATE TABLE bookings(
+  id SERIAL PRIMARY KEY,
+  room_id INTEGER REFERENCES rooms(id),
+  guests_id INTEGER REFERENCES guests(id),
+  check_in DATE,
+  check_out DATE
+);
