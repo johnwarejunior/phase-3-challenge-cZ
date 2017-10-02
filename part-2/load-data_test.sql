c4df0101df8a5067a1fb51b385ea6bb1a7645316
johnware15 <contact@johndware.com>
2017-10-02 16:59:25 -0700

    Load data code added for both main and test SQL files.

---
+++ part-2/load-data_test.sql
@@ -0,0 +1,15 @@
+\c hotel_db_test
+
+COPY guests (id, name, email) FROM '/Users/johnware/Desktop/learners-guild/phase-3-challenge-c/part-2/csvFiles/guests.csv' DELIMITER ',' CSV HEADER;
+
+COPY rooms (id, room_num, capacity) FROM '/Users/johnware/Desktop/learners-guild/phase-3-challenge-c/part-2/csvFiles/rooms.csv' DELIMITER ',' CSV HEADER;
+
+COPY bookings (id, room_id, guest_id, check_in, check_out) FROM '/Users/johnware/Desktop/learners-guild/phase-3-challenge-c/part-2/csvFiles/bookings.csv' DELIMITER ',' CSV HEADER;
+
+
+UPDATE rooms
+SET available = false
+FROM bookings
+WHERE bookings.check_in <= DATE '2017-08-27'
+AND bookings.check_out > DATE '2017-08-27'
+AND bookings.room_id = rooms.id;
