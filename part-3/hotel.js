2c9e58888c2a06b227ed82a3c716e7e0a5fadcee
johnware15 <contact@johndware.com>
2017-10-03 14:28:20 -0700

    Loop function for hotel modal

---
+++ part-3/hotel.js
@@ -0,0 +1,19 @@
+const book = document.getElementsByClassName('book')
+const modal = document.getElementById('booking')
+const close = document.getElementsByClassName('close')[0]
+const modalRoomNum = document.getElementById('roomNum')
+const modalPrice = document.getElementById('pricePerNight')
+const modalTotal = document.getElementById('total')
+const quantity = document.querySelector('#quantity')
+
+let room = []
+
+for (var i = 0; i < book.length; i++) {
+  let reserve = book[i]
+  reserve.addEventListener('click', function() {
+    room.num = reserve.parentNode.previousElementSibling.previousElementSibling.previousElementSibling.innerHTML;
+    room.capacity = reserve.parentNode.previousElementSibling.previousElementSibling.innerHTML;
+    room.price = reserve.parentNode.previousElementSibling.innerHTML;
+    bookingContent()
+  })
+}
